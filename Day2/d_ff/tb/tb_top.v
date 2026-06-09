03b56967af2352e625f4b666d7f49c473617980e
Ananthu2160 <ananthuak265@gmail.com>
2026-06-09 21:24:08 +0530

    Update tb_top.v

--- Day2/d_ff/tb/tb_top.v
+++ Day2/d_ff/tb/tb_top.v
@@ -1,1 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 14:12:50
+// Design Name: 
+// Module Name: d_ff_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module d_ff_tb;
+    reg d,clk;
+    wire q,qbar;
+    d_ff df(q,qbar,d,clk);
+    initial
+    begin
+    clk=1'b0;
+    forever #5 clk=~clk;
+    end
+    initial
+    begin
+    d=1'b0; #10;
+    d=1'b1; #10;
+    d=1'b0; #10;
+    d=1'b1; #10;
+    d=1'b0; #10;
+    $stop;
+    end
+endmodule
 
