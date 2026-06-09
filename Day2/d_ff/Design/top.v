c18dd31bab7c0d4f86847d8f9cf823956bf1d920
Ananthu2160 <ananthuak265@gmail.com>
2026-06-09 21:22:56 +0530

    Update top.v

--- Day2/d_ff/Design/top.v
+++ Day2/d_ff/Design/top.v
@@ -1,1 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 14:12:25
+// Design Name: 
+// Module Name: d_ff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module d_ff(q,qbar,d,clk);
+    input d,clk;
+    output reg q,qbar;
+    always@(posedge clk)begin
+        q<=d;
+        qbar<=~d;
+        end
+endmodule
 
