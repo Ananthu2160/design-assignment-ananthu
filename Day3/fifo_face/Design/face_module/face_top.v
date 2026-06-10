e12c406ae457e79e0e22d1ec9995c8b58563fcf5
Ananthu2160 <ananthuak265@gmail.com>
2026-06-10 22:19:00 +0530

    Update face_top.v

--- Day3/fifo_face/Design/face_module/face_top.v
+++ Day3/fifo_face/Design/face_module/face_top.v
@@ -1,1 +1,15 @@
+module face_module(
+input clk,
+input rst,
+output reg [7:0] data_out
+);
 
+always @(posedge clk)
+begin
+    if(rst)
+        data_out <= 8'd1;
+    else
+        data_out <= data_out + 1;
+end
+
+endmodule
