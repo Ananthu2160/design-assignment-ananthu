8a37b35780fcca7068b37535b60c22c3d47195b7
Ananthu2160 <ananthuak265@gmail.com>
2026-06-10 22:21:51 +0530

    Update tb_top.v

--- Day3/fifo_face/tb/tb_top.v
+++ Day3/fifo_face/tb/tb_top.v
@@ -1,1 +1,30 @@
+`timescale 1ns/1ps
 
+module top_tb;
+
+reg clk;
+reg rst;
+
+wire [7:0] fifo_data;
+
+top dut(
+    .clk(clk),
+    .rst(rst),
+    .fifo_data(fifo_data)
+);
+
+always #5 clk = ~clk;
+
+initial
+begin
+    clk = 0;
+    rst = 1;
+
+    #10;
+    rst = 0;
+
+    #200;
+    $finish;
+end
+
+endmodule
