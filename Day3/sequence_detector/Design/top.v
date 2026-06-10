9b34748e7cda865b4429ac44e2a5101df386d67a
Ananthu2160 <ananthuak265@gmail.com>
2026-06-10 22:01:47 +0530

    Update top.v

--- Day3/sequence_detector/Design/top.v
+++ Day3/sequence_detector/Design/top.v
@@ -1,1 +1,76 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 11:23:39
+// Design Name: 
+// Module Name: sequence2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sequence2(
+input clk,rst,din,
+output reg detected
+    );
+    parameter idle=2'b00;
+    parameter s1=2'b01;
+    parameter s2=2'b10;
+    parameter s3=2'b11;
+    reg [1:0]ps,ns;
+    always @(posedge clk)
+    begin
+    if(rst==1)begin
+    ps<=idle;
+    end
+    else
+    ps<=ns;
+    end
+    always @(*)
+    begin
+    case(ps)
+    idle:begin
+    detected=0;
+    if(din==0)begin
+    ns=ps;
+    end
+    else
+    ns=s1;
+    end
+    s1:begin
+    if(din==0)begin
+    ns=idle;
+    end
+    else
+    ns=s2;
+    end
+    s2:begin
+       if(din==0)begin
+       ns=idle;
+       end
+       else
+       ns=s3;
+       end
+       s3:begin
+          if(din==0)begin
+          ns=idle;
+          detected=1;
+          end
+          else
+          ns=s3;
+          end
+    endcase
+    end
+endmodule
 
