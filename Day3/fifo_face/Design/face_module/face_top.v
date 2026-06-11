90cbbc77422838e08bfac2670b7e45b9926eb940
Ananthu2160 <ananthuak265@gmail.com>
2026-06-11 21:33:07 +0530

    Update face_top.v

--- Day3/fifo_face/Design/face_module/face_top.v
+++ Day3/fifo_face/Design/face_module/face_top.v
@@ -1,15 +1,38 @@
-module face_module(
-input clk,
-input rst,
-output reg [7:0] data_out
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.06.2026 20:53:35
+// Design Name: 
+// Module Name: face_mod
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module face_mod(
+    input clk,
+    input rst,
+    input [7:0] s_in,
+    output reg [7:0] s_out
 );
 
-always @(posedge clk)
+always @(posedge clk or posedge rst)
 begin
-    if(rst)
-        data_out <= 8'd1;
+    if (rst)
+        s_out <= 0;
     else
-        data_out <= data_out + 1;
+        s_out <= s_in;   // you can replace with real logic
 end
 
 endmodule
